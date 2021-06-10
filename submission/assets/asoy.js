727ae4435125932d98f87b94defde52bfb6a1522
farrosilham <85678743+farrosilham@users.noreply.github.com>
2021-06-10 17:15:44 +0700

    Add files via upload

---
+++ submission/assets/asoy.js
@@ -0,0 +1,17 @@
+<script>
+function myFunction() {
+  document.getElementsByClassName("profil").style.transitionProperty = "width, height"
+}
+function myFunction() {
+  document.getElementsByClassName("tentang").style.transitionProperty = "width, height"
+}
+function myFunction() {
+  document.getElementsByClassName("keterampilan").style.transitionProperty = "width, height"
+}
+function myFunction() {
+  document.getElementsByClassName("tugas").style.transitionProperty = "width, height"
+}
+function myFunction() {
+  document.getElementsByClassName("kesimpulan").style.transitionProperty = "width, height"
+}
+</script>
